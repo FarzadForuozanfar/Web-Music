24a2cc3aff47ef7f252a08220a1e40006004510e
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2022-07-30 14:19:22 +0430

    Delete database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db = new mysqli("localhost","id19312075_farzad","[4LT_HSEk@Jk=l5%","id19312075_webmusic");
-    //$db = new mysqli("localhost","root","","webmusic");
-    if($db->connect_error)
-    {
-        echo $db->connect_error;
-    }
-?>
